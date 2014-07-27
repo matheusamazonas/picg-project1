85111500aa8b0d020355a899e1852f327b55bb2c
Matheus Andrade <matheus.amazonas@gmail.com>
2014-07-27 19:02:37 -0300

    User control done
    User control is done. Keyboard and mouse input are mapped to each desired behavior. There are some glitches probably occasioned by a bad choice of frustum
    
    Migrated all Position, Front and Up vector to GLM Vector3 representation. The same will be applied to all future Vector3s
    
    Added sources.txt file which contains some of the source (links) from websites I used as reference

--- main.cpp
+++ main.cpp
@@ -2,96 +2,122 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
+#define GLM_FORCE_RADIANS
+#include "glm/glm.hpp"
+#include "glm/gtc/matrix_transform.hpp"
+#include "shader.hpp"
+using namespace glm;
 
-float camPositionX = 0, camPositionY = 0, camPositionZ = 0;
-float camFrontX = 0, camFrontY = 0, camFrontZ = 1;
-float camUpX = 0, camUpY = 1, camUpZ = 0;
+vec3 cameraPos = vec3 (0.0f, 0.0f, -2.0f);
+vec3 cameraFront = vec3 (0.0f, 0.0f, 1.0f);
+vec3 cameraUp = vec3 (0.0f, 1.0f, 0.0f);
+
 float camSpeed = 0.05f;
-
-void init(void)
-{
-	// Light
-	/*
-	   GLfloat light_position[] = {1.0, 1.0, 1.0, 0.0};
-	   GLfloat light_diffuse[] = {1.0, 1.0, 1.0, 1.0};
-	   GLfloat light_ambient[] = {0.2, 0.2, 0.2, 1.0};
-	   glLightfv (GL_LIGHT0, GL_POSITION, light_position);
-	   glLightfv (GL_LIGHT0, GL_DIFFUSE, light_diffuse);
-	   glLightfv (GL_LIGHT0, GL_AMBIENT, light_ambient);
-	   glEnable(GL_LIGHTING);
-	   glEnable(GL_LIGHT0);	
-
-	   glEnable(GL_DEPTH_TEST);
-	 */
-
-}
+float mouseSpeed = 0.005f;
+GLFWwindow* window;
+// Initial horizontal angle : toward -Z
+float horizontalAngle = 3.14f;
+// Initial vertical angle : none
+float verticalAngle = 0.0f;
+float deltaTime;
+double windowSizeX = 800, windowSizeY = 600;
 
 void keyboard(GLFWwindow* window, int key, int scancode, int action, int mods)
 {
+	vec3 right = cross (cameraFront, cameraUp);
 	switch (key) 
 	{
 		case GLFW_KEY_W:
 			if (action == GLFW_PRESS || action == GLFW_REPEAT)
 			{
-				camPositionZ += camSpeed * camFrontZ;
-				printf("Pressed up\n");
+				cameraPos += cameraFront * deltaTime * camSpeed;
 			}
 			break;
 		case GLFW_KEY_S:
 			if (action == GLFW_PRESS || action == GLFW_REPEAT)
 			{
-				camPositionZ -= camSpeed * camFrontZ;
-				printf("Pressed down\n");
+				cameraPos -= cameraFront * deltaTime * camSpeed;
 			}
 			break;
 		case GLFW_KEY_A:
 			if (action == GLFW_PRESS || action == GLFW_REPEAT)
 			{
-				camPositionX += camSpeed;
-				printf("Pressed left\n");
+				cameraPos -= right * deltaTime * camSpeed;
 			}
 			break;
 		case GLFW_KEY_D:
 			if (action == GLFW_PRESS || action == GLFW_REPEAT)
 			{
-				camPositionX -= camSpeed;
-				printf("Pressed right\n");
+				cameraPos += right * deltaTime * camSpeed;
 			}
 			break;
 	}
-	printf("Camera position: {%f, %f, %f}\n", camPositionX, camPositionY, camPositionZ);
+	//printf("Camera position: {%f, %f, %f}\n", cameraPos.x, cameraPos.y, cameraPos.z);
+	printf("Camera front: {%f, %f, %f\n", cameraFront.x, cameraFront.y, cameraFront.z);
 }
 
+void mouse (GLFWwindow* window, double xPos, double yPos)
+{
+	vec3 right = vec3(
+			sin(horizontalAngle - 3.14f/2.0f), 
+			0,
+			cos(horizontalAngle - 3.14f/2.0f)
+			);	
+}
+
+void computeMatricesFromInputs(void)
+{
+	// glfwGetTime is called only once, the first time this function is called
+	static double lastTime = glfwGetTime();
+
+	// Compute time difference between current and last frame
+	double currentTime = glfwGetTime();
+	deltaTime = float(currentTime - lastTime);
+
+	// Get mouse position
+	double xpos, ypos;
+	glfwGetCursorPos(window, &xpos, &ypos);
+
+	// Compute new orientation
+	horizontalAngle = mouseSpeed * float(windowSizeX/2 - xpos);
+	verticalAngle   = mouseSpeed * float( windowSizeY/2 - ypos);
+
+	// Direction : Spherical coordinates to Cartesian coordinates conversion
+	cameraFront = vec3(
+			cos(verticalAngle) * sin(horizontalAngle), 
+			sin(verticalAngle),
+			cos(verticalAngle) * cos(horizontalAngle)
+			);
+}
 void display(void)
 {
 	glClear(GL_COLOR_BUFFER_BIT);
+
+	// Compute the MVP matrix from keyboard and mouse input
+	computeMatricesFromInputs();
 
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
 
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
-	glFrustum (-1.0, 1.0, -1.0, 1.0, 1.0, 20.0);
+	glFrustum (-1.0, 1.0, -1.0, 1.0, 1.0, 100.0);
 	//glOrtho(-1.0, 1.0, -1.0, 1.0, -1.0, 1.0);
-
-
 	gluLookAt(                        // Set camera position and orientation
-			camPositionX, camPositionY, camPositionZ,              			
-			camFrontX, camFrontY, camFrontZ,                // View point (x,y,z)
-			camUpX, camUpY, camUpZ                 // Up-vector (x,y,z)
+			cameraPos.x, cameraPos.y, cameraPos.z,              			
+			cameraFront.x, cameraFront.y, cameraFront.z,                // View point (x,y,z)
+			cameraUp.x, cameraUp.y, cameraUp.z                 // Up-vector (x,y,z)
 			);
 
 	//Mais sobre os comandos de push e pop matrices aqui: 
 	//http://www.opengl.org/sdk/docs/man2/xhtml/glPushMatrix.xml
 	//	glPushMatrix();
 
-
 	glColor3f(0.7, 0.7, 0.7);
 
 	//Mais sobre os comandos de push e pop matrices aqui: 
 	//http://www.opengl.org/sdk/docs/man2/xhtml/glPushMatrix.xml
 	//glPushMatrix();
-
 
 	static GLfloat vertices[] = {
 		-0.5, -0.5, 0.0, 
@@ -126,11 +152,25 @@ void display(void)
 	glFlush();
 }
 
+void init(void)
+{
+	// Light
+	/*
+	   GLfloat light_position[] = {1.0, 1.0, 1.0, 0.0};
+	   GLfloat light_diffuse[] = {1.0, 1.0, 1.0, 1.0};
+	   GLfloat light_ambient[] = {0.2, 0.2, 0.2, 1.0};
+	   glLightfv (GL_LIGHT0, GL_POSITION, light_position);
+	   glLightfv (GL_LIGHT0, GL_DIFFUSE, light_diffuse);
+	   glLightfv (GL_LIGHT0, GL_AMBIENT, light_ambient);
+	   glEnable(GL_LIGHTING);
+	   glEnable(GL_LIGHT0);
+
+	   glEnable(GL_DEPTH_TEST);
+	 */
+}
 
 int main (int argc, char** argv) 
 {
-	GLFWwindow* window;
-
 	/* Initialize the library */
 	if (!glfwInit())
 		return -1;
@@ -143,7 +183,7 @@ int main (int argc, char** argv)
 	//glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 
 	/* Create a windowed mode window and its OpenGL context */
-	window = glfwCreateWindow(512, 512, "Example 1-1", NULL, NULL);
+	window = glfwCreateWindow(windowSizeX, windowSizeY, "Example 1-1", NULL, NULL);
 	if (!window)
 	{
 		printf("Error while creating window\n");
@@ -152,13 +192,20 @@ int main (int argc, char** argv)
 	}
 
 	/* Make the window's context current */
-	glfwMakeContextCurrent(window);
+	glfwMakeContextCurrent (window);
+	glfwSetCursorPos (window, windowSizeX/2, windowSizeY/2);
 	glfwSetKeyCallback (window, keyboard);
-	glfwSwapInterval(1);
+	glfwSetCursorPosCallback (window, mouse); 
+	glfwSwapInterval (1);
 
 	// Initializes the GLEW library
 	glewExperimental = GL_TRUE;
-	glewInit();
+	if (glewInit() != GLEW_OK)
+	{
+		printf("Failed to initialize GLEW\n");
+		return -1;
+	}
+
 
 	// Prints the OpenGL Version on the terminal
 	//printf("OpenGL Version: %s\n", glGetString(GL_VERSION));
